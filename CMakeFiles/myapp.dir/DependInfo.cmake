e70f3a6de324c3eaf893d6d2ebf0c8ee154a0c97
Dmytro Kiro <dimakiro2105@gmail.com>
2019-09-23 10:13:30 -0400

    Made project to work with relative path

---
+++ CMakeFiles/myapp.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dkiro/CLionProjects/untitled/src/main.cpp" "/Users/dkiro/CLionProjects/untitled/CMakeFiles/myapp.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "glad/include"
+  "/usr/local/Cellar/glfw/3.3/include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/dkiro/CLionProjects/untitled/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
